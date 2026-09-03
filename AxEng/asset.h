3fc07a855c08dcf8644d9c56d6786dd68409640e
Elliot <elliotharris@live.co.uk>
2026-09-03 18:46:06 +0100

    Remove tagging assets as outdated
    Might replace later, needs a proper mechanism now assets aren't static

--- AxEng/asset.h
+++ AxEng/asset.h
@@ -13,14 +13,11 @@ namespace ax
 		virtual ~Asset() = default;
 
 		inline bool is_loaded() const { return m_loaded; }
-		inline bool is_outdated() const { return m_outdated; }
 
-		void tag_outdated() { m_outdated = true; }
 		const std::string& asset_name() const { return m_name; }
 
 	protected:
 		Asset(const std::string& name);
-		bool m_outdated{ false };
 		bool m_loaded{ false };
 		const std::string m_name;
 	};
